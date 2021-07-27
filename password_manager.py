7c9a1bac073017be75403124b0c1d69b2cf4d7db
rmill <rmiller003@gmail.com>
2021-07-26 22:24:40 -0400

    Stage 1

---
+++ password_manager.py
@@ -0,0 +1,21 @@
+
+pwd = input("What is the master password")
+
+def view():
+    pass
+
+def add():
+    pass
+
+while True:
+    mode = input("Would you like to add a new password or view existing ones (view, add), press q to quit? ").lower()
+    if mode == "q":
+        break
+
+    if mode == "view":
+        view()
+    elif mode == "add":
+        add()
+    else:
+        print("invalid mode")
+        continue
